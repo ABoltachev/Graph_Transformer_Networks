19be5f70b0955098909e7e5b55ee2f71bcb79b9e
Aleksandr Boltachev <boltachev.alex@gmail.com>
2020-03-11 01:57:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from model import GTN
 import pdb
 import pickle
 import argparse
-from utils import f1_score
+from utils import accuracy
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -50,21 +50,21 @@ if __name__ == '__main__':
 
     for i,edge in enumerate(edges):
         if i ==0:
-            A = torch.from_numpy(edge.todense()).type(torch.FloatTensor).unsqueeze(-1)
+            A = torch.from_numpy(edge.todense()).type(torch.cuda.FloatTensor).unsqueeze(-1)
         else:
-            A = torch.cat([A,torch.from_numpy(edge.todense()).type(torch.FloatTensor).unsqueeze(-1)], dim=-1)
-    A = torch.cat([A,torch.eye(num_nodes).type(torch.FloatTensor).unsqueeze(-1)], dim=-1)
+            A = torch.cat([A,torch.from_numpy(edge.todense()).type(torch.cuda.FloatTensor).unsqueeze(-1)], dim=-1)
+    A = torch.cat([A,torch.eye(num_nodes).type(torch.cuda.FloatTensor).unsqueeze(-1)], dim=-1)
     
-    node_features = torch.from_numpy(node_features).type(torch.FloatTensor)
-    train_node = torch.from_numpy(np.array(labels[0])[:,0]).type(torch.LongTensor)
-    train_target = torch.from_numpy(np.array(labels[0])[:,1]).type(torch.LongTensor)
-    valid_node = torch.from_numpy(np.array(labels[1])[:,0]).type(torch.LongTensor)
-    valid_target = torch.from_numpy(np.array(labels[1])[:,1]).type(torch.LongTensor)
-    test_node = torch.from_numpy(np.array(labels[2])[:,0]).type(torch.LongTensor)
-    test_target = torch.from_numpy(np.array(labels[2])[:,1]).type(torch.LongTensor)
+    node_features = torch.from_numpy(node_features).type(torch.cuda.FloatTensor)
+    train_node = torch.from_numpy(np.array(labels[0])[:,0]).type(torch.cuda.LongTensor)
+    train_target = torch.from_numpy(np.array(labels[0])[:,1]).type(torch.cuda.LongTensor)
+    valid_node = torch.from_numpy(np.array(labels[1])[:,0]).type(torch.cuda.LongTensor)
+    valid_target = torch.from_numpy(np.array(labels[1])[:,1]).type(torch.cuda.LongTensor)
+    test_node = torch.from_numpy(np.array(labels[2])[:,0]).type(torch.cuda.LongTensor)
+    test_target = torch.from_numpy(np.array(labels[2])[:,1]).type(torch.cuda.LongTensor)
     
     num_classes = torch.max(train_target).item()+1
-    final_f1 = 0
+    final_acc = 0
     for l in range(1):
         model = GTN(num_edge=A.shape[-1],
                             num_channels=num_channels,
@@ -73,6 +73,7 @@ if __name__ == '__main__':
                             num_class=num_classes,
                             num_layers=num_layers,
                             norm=norm)
+        model.cuda()
         if adaptive_lr == 'false':
             optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.001)
         else:
@@ -86,9 +87,9 @@ if __name__ == '__main__':
         best_val_loss = 10000
         best_test_loss = 10000
         best_train_loss = 10000
-        best_train_f1 = 0
-        best_val_f1 = 0
-        best_test_f1 = 0
+        best_train_acc = 0
+        best_val_acc = 0
+        best_test_acc = 0
         
         for i in range(epochs):
             for param_group in optimizer.param_groups:
@@ -98,28 +99,29 @@ if __name__ == '__main__':
             model.zero_grad()
             model.train()
             loss,y_train,Ws = model(A, node_features, train_node, train_target)
-            train_f1 = torch.mean(f1_score(torch.argmax(y_train.detach(),dim=1), train_target, num_classes=num_classes)).cpu().numpy()
-            print('Train - Loss: {}, Macro_F1: {}'.format(loss.detach().cpu().numpy(), train_f1))
+            train_acc = accuracy(torch.argmax(y_train.detach(), dim=1), train_target)
+            print('Train - Loss: {}, Acc: {}'.format(loss.detach().cpu().numpy(), train_acc))
             loss.backward()
             optimizer.step()
             model.eval()
             # Valid
             with torch.no_grad():
                 val_loss, y_valid,_ = model.forward(A, node_features, valid_node, valid_target)
-                val_f1 = torch.mean(f1_score(torch.argmax(y_valid,dim=1), valid_target, num_classes=num_classes)).cpu().numpy()
-                print('Valid - Loss: {}, Macro_F1: {}'.format(val_loss.detach().cpu().numpy(), val_f1))
+                val_acc = accuracy(torch.argmax(y_valid.detach(), dim=1), valid_target)
+                print('Valid - Loss: {}, Acc: {}'.format(val_loss.detach().cpu().numpy(), val_acc))
                 test_loss, y_test,W = model.forward(A, node_features, test_node, test_target)
-                test_f1 = torch.mean(f1_score(torch.argmax(y_test,dim=1), test_target, num_classes=num_classes)).cpu().numpy()
-                print('Test - Loss: {}, Macro_F1: {}\n'.format(test_loss.detach().cpu().numpy(), test_f1))
-            if val_f1 > best_val_f1:
+                test_acc = accuracy(torch.argmax(y_test.detach(), dim=1), test_target)
+                print('Test - Loss: {}, Acc: {}\n'.format(test_loss.detach().cpu().numpy(), test_acc))
+            if val_acc > best_val_acc:
                 best_val_loss = val_loss.detach().cpu().numpy()
                 best_test_loss = test_loss.detach().cpu().numpy()
                 best_train_loss = loss.detach().cpu().numpy()
-                best_train_f1 = train_f1
-                best_val_f1 = val_f1
-                best_test_f1 = test_f1 
+                best_train_acc = train_acc
+                best_val_acc = val_acc
+                best_test_acc = test_acc 
+            torch.cuda.empty_cache()
         print('---------------Best Results--------------------')
-        print('Train - Loss: {}, Macro_F1: {}'.format(best_train_loss, best_train_f1))
-        print('Valid - Loss: {}, Macro_F1: {}'.format(best_val_loss, best_val_f1))
-        print('Test - Loss: {}, Macro_F1: {}'.format(best_test_loss, best_test_f1))
-        final_f1 += best_test_f1
+        print('Train - Loss: {}, Acc: {}'.format(best_train_loss, best_train_acc))
+        print('Valid - Loss: {}, Acc: {}'.format(best_val_loss, best_val_acc))
+        print('Test - Loss: {}, Acc: {}'.format(best_test_loss, best_test_acc))
+        final_acc += best_test_acc
